ad206973fe43ec480a2bb7a7fbd7160a70e89119
KleKoR <Klekor@hotmail.com>
2020-12-19 20:32:52 +0200

    v0.3

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,95 +1,14 @@
-#xsubs.tv
+# coding=utf-8
+import sys
+from datetime import datetime, timedelta
 
 import string, os, re #urllib, zipfile, copy
-
+from guessit import guessit
+import dexml
+from dexml import fields
 OS_PLEX_USERAGENT = 'plexapp.com v9.0'
 
 seriesList = {}
-
-
-resolDict = {
-    '720p' : ['HDTV', 'BluRay', 'WEB-DL', 'WEBRip', 'HDDVD', 'BRRip', 'WEB-Rip'],
-    '1080p' : ['BluRay', 'WEB-DL', 'WEBRip', 'WS', 'HDTV'],
-    '480p' : ['HDTV', 'WEB-DL', 'BluRay', 'WEBRip'],
-    '1080i' : ['HDTV'],
-    '576p' : ['HDTV']
-}
-
-sourceDict = {
-    'dvdrip' : ['x264', 'XviD'],
-    'hdtv' : ['720p', 'XviD', 'x264', '480p', '1080i', '1080p', 'DivX', '576p'],
-    'bluray' : ['720p', '1080p', '480p', 'XviD'],
-    'web-dl' : ['720p', '1080p', '480p', 'x264', 'XviD', 'H264'],
-    'webrip' : ['720p', '1080p', 'x264', 'XviD', '480p'],
-    'pdtv' : ['HR', 'x264', 'XviD'],
-    'bdrip' : ['x264', 'XviD', 'WS'],
-    'tvrip' : [],
-    'hddvd' : ['720p'],
-    'dvdscr' : [],
-    'web-rip' : ['720p'],
-    'ws' : ['1080p', 'BDRip', 'XviD']
-}
-
-encDict = {
-    'xvid' : ['HDTV', 'DVDRip', 'BDRip', 'WEBRip', 'WEB-DL', 'PDTV', 'AC3', 'WS', 'BluRay'],
-    'x264' : ['HDTV', 'DVDRip', 'BDRip', 'WEBRip', 'WEB-DL', 'PDTV'],
-    'h264' : ['WEB-DL'],
-    'divx' : ['HDTV']
-}
-
-encTypesReplaceDict = {
-    'dvdrip' : 'DVDRip',
-    'hdtv' : 'HDTV',
-    'xvid' : 'XviD',
-    'x264' : 'x264',
-    'bluray' : 'BluRay',
-    'web-dl' : 'WEB-DL',
-    'webrip' : 'WEBRip',
-    'pdtv' : 'PDTV',
-    'bdrip' : 'BDRip',
-    'dsr' : 'DSR',
-    'ws' : 'WS',
-    'brrip' : 'BRRip',
-    'hr' : 'HR',
-    'tvrip' : 'TVRip',
-    'hddvd' : 'HDDVD',
-    'h264' : 'H264',
-    'dvdscr' : 'DVDSCR',
-    'ac3' : 'AC3',
-    'divx' : 'DivX',
-    'web-rip' : 'WEB-Rip'
-}
-
-#unused
-encTypesDict = {
-    'DVDRip' : ['x264', 'XviD'],
-    '720p' : ['HDTV', 'BluRay', 'WEB-DL', 'WEBRip', 'HDDVD', 'BRRip', 'WEB-Rip'],
-    'HDTV' : ['720p', 'XviD', 'x264', '480p', '1080i', '1080p', 'DivX', '576p'],
-    'XviD' : ['HDTV', 'DVDRip', 'BDRip', 'WEBRip', 'WEB-DL', 'PDTV', 'AC3', 'WS', 'BluRay'],
-    'x264' : ['HDTV', 'DVDRip', 'BDRip', 'WEBRip', 'WEB-DL', 'PDTV'],
-    'BluRay' : ['720p', '1080p', '480p', 'XviD'],
-    'WEB-DL' : ['720p', '1080p', '480p', 'x264', 'XviD', 'H264'],
-    'WEBRip' : ['720p', '1080p', 'x264', 'XviD', '480p'],
-    '1080p' : ['BluRay', 'WEB-DL', 'WEBRip', 'WS', 'HDTV'],
-    '480p' : ['HDTV', 'WEB-DL', 'BluRay', 'WEBRip'],
-    'PDTV' : ['HR', 'x264', 'XviD'],
-    'BDRip' : ['x264', 'XviD', 'WS'],
-    'DSR' : [],
-    'WS' : ['1080p', 'BDRip', 'XviD'],
-    '1080i' : ['HDTV'],
-    'BRRip' : ['720p'],
-    'HR' : ['PDTV'],
-    'TVRip' : [],
-    'HDDVD' : ['720p'],
-    'H264' : ['WEB-DL'],
-    'DVDSCR' : [],
-    'AC3' : ['XviD'],
-    'DivX' : ['HDTV'],
-    'WEB-Rip' : ['720p'],
-    '576p' : ['HDTV']
-}
-
-dctTeam = {}
 
 def Start():
     HTTP.CacheTime = 0
@@ -99,177 +18,90 @@ def Start():
 def ValidatePrefs():
     return
 
+
+# def logObject(obj):
+#     Log("obj: %s" % str(obj))
+#     for attr in dir(obj):
+#         Log("obj.%s = %r" % (attr, getattr(obj, attr)))
+
+class sr(dexml.Model):
+    rlsid = fields.String()
+    team = fields.String(tagname='team')
+    fmt = fields.String(tagname='fmt')
+    hits = fields.Integer(tagname='hits')
+    duration_string = fields.String(tagname='duration')   
+    dur=None
+    
+    def get_duration(self):
+        if self.dur is None:
+            try:
+                timet1 = datetime.strptime(self.duration_string,"%H:%M:%S")
+            except ValueError:
+                timet1 = datetime.strptime(self.duration_string,"%M:%S")
+            delta1 = timedelta(
+                hours=timet1.time().hour,
+                minutes=timet1.time().minute,
+                seconds=timet1.time().second)
+            self.dur = delta1.total_seconds()*1000
+        return self.dur
+    duration = property(get_duration)
+        
+
+
 def getSubUrl(data):
-    Log('Filename:  '+ data['sFl'])
-    sre = getSourceResolutionEncoding(data['sFl'])
-    fnlName = findSeriesNameinXsubs(data['sK'])
-    if fnlName !='':
-        srid = seriesList[fnlName]
+    show_title = findSeriesNameinXsubs(data.show_title)
+    if show_title !='':
+        srid = seriesList[show_title]
     else:
+        Log("Couldn't find series in X-Subs. Plex Series Name: %s" % data.show_title)
         return
 
-    Log(srid)
     seriesUrl = 'http://xsubs.tv/series/'+str(srid)+'/main.xml'
     Log(seriesUrl)
-    elem = HTML.ElementFromURL(seriesUrl)
-    subpages = elem.xpath("//series_group[@ssnnum='"+data['sTS']+"']/@ssnid")
-    Log(subpages)
-    Log("episode: "+data['sTE'])
-    if len(subpages)==1:
-        Log(data['sTS'])
-        ssnUrl = 'http://xsubs.tv/series/'+str(srid)+'/'+subpages[0]+'.xml'
-        ssnElem = HTML.ElementFromURL(ssnUrl)
-        #First try get 
-        #//subg[etitle[@number=4]]//sr[@published_on!="" and team/text()='REPACK KILLERS' and fmt/text()='HDTV.x264']//@rlsid
-        fmt  = getAppropriateFmt(sre)
-        Log(fmt)
-        Log("//subg[etitle[@number="+data['sTE']+"]]//sr[@published_on!='' and team/text()='"+data['sR']+"' and fmt/text()='"+fmt+"']//@rlsid")
-        firsttry = ssnElem.xpath("//subg[etitle[@number="+data['sTE']+"]]//sr[@published_on!='' and team/text()='"+data['sR']+"' and fmt/text()='"+fmt+"']//@rlsid")
-        Log(firsttry)
-        if not firsttry:
-            Log(' not firsttry')
-            if ('proper' in data['sFl'].lower()) or ('repack' in data['sFl'].lower()):
-                #trysearch without proper or repack
-                Log("if 'PROPER' or 'REPACK' in data['sFl']:")
-                rlsGroup = string.replace(data['sR'], 'REPACK','')
-                rlsGroup = string.replace(rlsGroup,'PROPER','').strip()
-                Log(data['sFl'])
-                Log(rlsGroup)
-                Log("//subg[etitle[@number="+data['sTE']+"]]//sr[@published_on!='' and team/text()='"+rlsGroup+"' and fmt/text()='"+fmt+"']//@rlsid")
-                secondtry = ssnElem.xpath("//subg[etitle[@number="+data['sTE']+"]]//sr[@published_on!='' and team/text()='"+rlsGroup+"' and fmt/text()='"+fmt+"']//@rlsid")
-                Log(secondtry)
-                if secondtry:
-                    return 'http://xsubs.tv/xthru/getsub/'+ secondtry[0]
-            else:
-                rlsGroup = data['sR']
-                rlsGroup = 'PROPER ' + rlsGroup
-                Log(rlsGroup)
-                Log("//subg[etitle[@number="+data['sTE']+"]]//sr[@published_on!='' and team/text()='"+rlsGroup+"' and fmt/text()='"+fmt+"']//@rlsid")
-                secondtry = ssnElem.xpath("//subg[etitle[@number="+data['sTE']+"]]//sr[@published_on!='' and team/text()='"+rlsGroup+"' and fmt/text()='"+fmt+"']//@rlsid")
-                Log(secondtry)
-                if secondtry:
-                    return 'http://xsubs.tv/xthru/getsub/'+ secondtry[0]
-                rlsGroup =  string.replace(rlsGroup, 'PROPER', 'REPACK')
-                Log("//subg[etitle[@number="+data['sTE']+"]]//sr[@published_on!='' and team/text()='"+rlsGroup+"' and fmt/text()='"+fmt+"']//@rlsid")
-                secondtry = ssnElem.xpath("//subg[etitle[@number="+data['sTE']+"]]//sr[@published_on!='' and team/text()='"+rlsGroup+"' and fmt/text()='"+fmt+"']//@rlsid")
-                Log(secondtry)
-                if secondtry:
-                    return 'http://xsubs.tv/xthru/getsub/'+ secondtry[0]
-            Log("Not Found")
-        else:
-            return 'http://xsubs.tv/xthru/getsub/'+ firsttry[0]
-        #xpath= //subg[etitle[@number=6]]/sr[team/text()='DIMENSION']/@rlsid
-        subNode = ssnElem.xpath('//subg[etitle[@number='+data['sTE']+']]//sr[@published_on!=""]')
-        Log('//subg[etitle[@number='+data['sTE']+']]//sr[@published_on!=""]')
-        for lala in subNode:
-            info = {}
-            info['fmt'] = lala.xpath('fmt/text()')[0]
-            info['hits'] = lala.xpath('hits/text()')[0]
-            #//@rlsid
-            info['rlsid'] = lala.xpath('./@rlsid')
-            if lala.xpath('team/text()')[0] in dctTeam:
-                dctTeam[lala.xpath('team/text()')[0]].append(info)
-            else:
-                dctTeam[lala.xpath('team/text()')[0]]= [info]
-        for key, value in dctTeam.iteritems():
-            Log('Key: '+key + '   Value= ')
-            Log(value)
-            if key.lower() == data['sR'].lower():
-                for vl in value:
-                    if vl['fmt'] == fmt:
-                        Log("//subg[etitle[@number="+data['sTE']+"]]//sr[@published_on!='' and team/text()='"+key+"' and fmt/text()='"+fmt+"']//@rlsid")
-                        secondtry = ssnElem.xpath("//subg[etitle[@number="+data['sTE']+"]]//sr[@published_on!='' and team/text()='"+key+"' and fmt/text()='"+fmt+"']//@rlsid")
-                        Log(secondtry)
-                        if secondtry:
-                            return 'http://xsubs.tv/xthru/getsub/'+ secondtry[0]
 
-def fillSeriesList():
-    #http://xsubs.tv/series/all.xml
+    elem = HTML.ElementFromURL(seriesUrl)    
+    subpages = elem.xpath("//series_group[@ssnnum='"+data.season+"']/@ssnid")
+
+    Log("Retrieving Subtitles")
+    ssnUrl = 'http://xsubs.tv/series/'+str(srid)+'/'+subpages[0]+'.xml'
+    ssnElem = HTML.ElementFromURL(ssnUrl)
+    subsElements = ssnElem.xpath("//subg[etitle[@number="+data.episode+"]]//sr[@published_on!='']")
+    
+    Log("Found %d subtitles" % len(subsElements))
+    Log("Searching subtitles for source: %s | resolution: %s | release_group: %s | duration: %s" % (data.source, data.resolution, data.release_group, data.duration))
+    srs = map(lambda x: sr.parse(HTML.StringFromElement(x)),subsElements)
+    
+    Log("Searching explicit subtitle results")
+    srs_filterd = filter(lambda x: (data.source in x.fmt.lower() and data.resolution in x.fmt.lower() and data.release_group in x.team.lower() and data.duration==x.duration)  ,srs)
+    if len(srs_filterd) > 0:
+        srs_filterd.sort(key=lambda x: x.hits, reverse=True)
+        Log("Found %d explicit matching subtitles" % len(srs_filterd))
+        for srf in srs_filterd:
+            Log("rlsid: %s | team: %s | fmt: %s  | hits: %s | duration: %s" % (srf.rlsid, srf.team, srf.fmt, srf.hits, srf.duration))
+    else:
+        Log("Searching fuzzy subtitle results")
+        srs_filterd = filter(lambda x: ((data.source in x.fmt.lower() or data.resolution in x.fmt.lower()) and data.release_group==x.team) or ((data.source in x.fmt.lower() or data.resolution in x.fmt.lower()) and data.duration==x.duration) ,srs)
+        srs_filterd.sort(key=lambda x: x.hits, reverse=True)
+        Log("Found %d fuzzy matching subtitles" % len(srs_filterd))
+        for srf in srs_filterd:
+            Log("rlsid: %s | team: %s | fmt: %s  | hits: %s | duration: %s" % (srf.rlsid, srf.team, srf.fmt, srf.hits, srf.duration))
+    if len(srs_filterd) > 0:
+        return 'http://xsubs.tv/xthru/getsub/'+ srs_filterd[0].rlsid
+    return ""
+    
+
+def RetrieveSeriesList():
+    Log("Retrieving Series list from X-Subs")
     seriesUrl = 'http://xsubs.tv/series/all.xml'
     elem = HTML.ElementFromURL(seriesUrl)
     subpages = elem.xpath("//series")
     for l in subpages:
         seriesList[l.xpath('./text()')[0]] = int(l.xpath('./@srsid')[0])
+    Log("Retrieved All Series successfully")
 
-def getReleaseGroup(filename):
-    tmpFile = string.replace(filename, '-', '.')
-    splitName = string.split(tmpFile, '.')
-    if ('gttvsd' in splitName[-2].lower()) or ('gtrd'in splitName[-2].lower()) or ('eztv'in splitName[-2].lower()) or ('vtv'in splitName[-2].lower()):
-        group = splitName[-3].strip()
-    else:
-        group = splitName[-2].strip() 
-    if 'REPACK' in filename:
-        group = 'REPACK '+ group
-    if 'PROPER' in filename:
-        group = 'PROPER ' + group
-    Log("group= " + group)
-    return group
-
-def getSourceResolutionEncoding(filename):
-    tmpFile = filename.lower()
-    splitName = string.split(tmpFile, '.')
-    retval = {'Source' : '','Resolution' : '','Encoding' : ''}
-    for l in splitName:
-        if l in sourceDict:
-            Log("Source in SourceDict: " + l)
-            retval['Source'] = encTypesReplaceDict[l]
-        if l in resolDict:
-            retval['Resolution'] = l
-        if l in encDict:
-            retval['Encoding'] = encTypesReplaceDict[l]
-    if retval['Encoding'] == '':
-        if '264' in tmpFile:
-            retval['Encoding'] = 'x264'
-        else:
-            Log("if '264' in tmpFile: = FALSE")
-            tmpFile = string.replace(filename.lower(), '-', '.')
-            splitName = string.split(tmpFile, '.')
-            for l in splitName:
-                if l in encDict:
-                    retval['Encoding'] = encTypesReplaceDict[l]
-    if retval['Source'] == '':
-        Log("if retval['Source'] == '':")
-        tmpFile = string.replace(filename.lower(), '-', '.')
-        splitName = string.split(tmpFile, '.')
-        for l in splitName:
-            if l in sourceDict:
-                retval['Source'] = encTypesReplaceDict[l]
-    if retval['Resolution'] == '':
-        Log("if retval['Resolution'] == '':")
-        tmpFile = string.replace(filename.lower(), '-', '.')
-        splitName = string.split(tmpFile, '.')
-        for l in splitName:
-            if l in resolDict:
-                retval['Resolution'] = encTypesReplaceDict[l]
-    Log("Source: %s" % retval['Source'])
-    Log("Resolution: %s" % retval['Resolution'])
-    Log("Encoding: %s" % retval['Encoding'])
-    return retval
-
-#according to xsubs site
-def getAppropriateFmt(fmtValues):
-    retval = ''
-    if fmtValues['Resolution'] != '':
-        retval = fmtValues['Resolution'] + '.' + fmtValues['Source']
-        Log("Fmt value: %s" % retval)
-        return retval
-    else:
-        if fmtValues['Source']!= '' and fmtValues['Encoding'] != '':
-            retval = fmtValues['Source'] + '.' + fmtValues['Encoding']
-            Log("Fmt value: %s" % retval)
-            return retval
-        if fmtValues['Source']== '':
-            retval =  fmtValues['Encoding']
-            Log("Fmt value: %s" % retval)
-            return retval
-        else:
-            retval =  fmtValues['Source']
-            Log("Fmt value: %s" % retval)
-            return retval
-    Log("Fmt value: %s" % retval)
-    return retval
 
 def findSeriesNameinXsubs(name):
+    Log("Searching the Plex Series Name: %s in the X-Subs Series List" % name)
     # try removing parenthesees in given name
     tmpName = name
     tmpName = re.sub(r'\([^)]*\)', '', tmpName).strip()
@@ -287,11 +119,11 @@ def findSeriesNameinXsubs(name):
     else:
         fnlName = tmpName
     if fnlName in seriesList:
-        Log('1 Original name: ' + name + '    found name: ' + fnlName)
+        Log("Found series in X-Subs with X-Subs Name: %s" % fnlName)
         return fnlName
     for key, value in seriesList.iteritems():
         if re.sub(r'\[[^)]*\]', '', key).lower().strip() == fnlName.lower():
-            Log('2 Original name: ' + name + '    found name: ' + key)
+            Log("Found series in X-Subs with X-Subs Name: %s" % key)
             return key
 
 
@@ -315,13 +147,40 @@ def findSeriesNameinXsubs(name):
             count = len(value)
             hgName = key
     if count > 1:
-        Log('3 Original name: ' + name + '    found name: ' + hgName)
+        Log("Found series in X-Subs with X-Subs Name: %s" % hgName)
         return hgName
     return ''
 
 
 
+class EpisodeData(object):
+  def __init__(self, show_title, season, episode, duration, resolution, source, release_group):
+    self.show_title = show_title
+    self.season = season
+    self.episode = episode
+    delta2 = timedelta(milliseconds=int(duration))
+    delta3 = timedelta(
+        seconds=delta2.seconds)
+    self.duration = delta3.total_seconds()*1000
+    self.resolution = resolution
+    self.source = source.lower()
+    self.release_group = release_group.lower()
 
+def LoggedIn():
+    forum = HTML.ElementFromURL("http://xsubs.tv/xforum/")
+    a = forum.xpath("//a[@href='/xforum/account/signin/']")
+    if len(a) > 0:
+        return False
+    return True
+
+def LogIn():
+    forum = HTML.ElementFromURL("http://xsubs.tv/xforum/account/signin/")
+    csrfmiddlewaretoken = forum.xpath("//input[@name='csrfmiddlewaretoken']/@value")[0]
+    values = {'csrfmiddlewaretoken' : csrfmiddlewaretoken,
+          'username' : Prefs["xsubs.username"],
+          'password' : Prefs["xsubs.password"] ,
+          'next' : '' }
+    forum = HTML.ElementFromURL("http://xsubs.tv/xforum/account/signin/", values, method="POST")
 
 class XsubsSubtitlesAgentTvShows(Agent.TV_Shows):
     name = 'Xsubs TV Subtitles'
@@ -335,35 +194,38 @@ class XsubsSubtitlesAgentTvShows(Agent.TV_Shows):
 
     def update(self, metadata, media, lang):
         Log("TvUpdate. Lang %s" % lang)
+        #logObject(media)
         for season in media.seasons:
             for episode in media.seasons[season].episodes:
                 for item in media.seasons[season].episodes[episode].items:
-                    Log("show: %s" % media.title)
-                    Log("Season: %s, Ep: %s" % (season, episode))
+                    Log("Searching xsubs for Show: %s, Season: %s, Ep: %s" % (media.title, season, episode))
                     for part in item.parts:
-                        Log("Release group: %s" % getReleaseGroup(part.file))
-                        data = {}
-                        data['sK'] = media.title
-                        data['sTS'] = season
-                        data['sTE'] = episode
-                        data['sR'] = getReleaseGroup(part.file)
-                        data['sFl'] = part.file
+                        Log("Filename: %s" % os.path.basename(part.file))
+                        guess = guessit(os.path.basename(part.file))
+                        Log("Guess: %s" % guess)
+                        if (Prefs["xsubs.username"]) and (Prefs["xsubs.password"]):
+                            Log("X-Subs account credentials has been set.")
+                            if not LoggedIn():
+                                Log("X-Subs account is not logged in, try to Login")
+                                LogIn()
+                            else:
+                                Log("X-Subs account is logged in")
+                        else:
+                            Log("X-Subs account credentials has not been set.")
 
-                        fillSeriesList()
+                        RetrieveSeriesList()
 
-                        subUrl = getSubUrl(data)
+                        episode_data = EpisodeData(media.title, season,episode, part.duration ,guess["screen_size"],guess["source"],guess["release_group"] )
+                        subUrl = getSubUrl(episode_data)
                         if not subUrl:
                             Log('Subtitle URL not found')
                             return
                         Log('Subtitle URL: '+subUrl)
                         language = 'ell'
-                        
-                        if language in part.subtitles:
-                            if subUrl in part.subtitles[language]:
-                                return
-                        Log("ready to download")
-                        Log(language)
-
-                        part.subtitles[Locale.Language.Match(language)][subUrl+'xsubs'] = Proxy.Media(HTTP.Request(subUrl), codec='srt', format='xsubs.srt')
+                        Log("Ready to download")
+                        #subtitle = Proxy.Media(HTTP.Request(subUrl), ext='srt', format='srt')
+                        subtitle = Proxy.Media(HTTP.Request(subUrl), ext='srt')
+                        part.subtitles[Locale.Language.Match(language)]['xsubs'] = subtitle
+                        Log("Downloaded")
 
 
--- README.md
+++ README.md
@@ -1,10 +1,30 @@
-Xsubs.bundle
+Xsubs TV Subtitles
 ============
 
 Greek subtitles agent plugin for Plex Media Server using the Xsubs.tv site.
 
-Uses xsubs.srt extension so it's not overriding existing greek subtitle.
-
-Installation Instructions:
 Download and extract the zip folder at plugins directory of Plex Media Server.
 Windows: C:\Users\username\AppData\Local\Plex Media Server\Plug-ins\
+## Installation
+============
+
+* Grab the [latest release](https://github.com/pannal/Xsubs.bundle/releases/latest)
+* unpack it
+* place the `Xsubs.bundle` folder inside Plug-ins folder, Windows: `C:\Users\{user}\AppData\Local\Plex Media Server\Plug-ins\`, `~/Library/Application Support/Plex Media Server/Plug-ins/`
+* restart your Plex Media Server.
+
+Additionally, you need to enable the plugin for the library:
+- go to Settings -> Server -> Agents -> TV Shows.
+- select the TheTVDB metadata provider on your library.
+- enable Xsubs TV Subtitles
+- configure username/password for http://xsubs.tv/xforum/ account, it is necessary otherwise there is a limit, 5 downloads/45 minutes for unregistered users.
+- refresh your library (or individual TV shows)
+
+
+## Changelog
+============
+o.3
+
+* Rewrite from scratch.
+* Add optional authentication to xsubs.tv site. 
+* Remove xsubs.srt extension causing plex always transcode.
